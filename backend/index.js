ac421e45e5f45650f08b34675401a35f628d1da1
UmarAbdulah <uabdullah204@gmail.com>
2026-01-13 23:48:00 +0500

    Graphql folder structure

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+
+// graphql schema defines the structure of the data that clients can query and operation they can perform
+// this schema consists of 2 main parts 
+// type defs 
+// this defines tha shape of the data avaialable in the graphql api.they specify the type of the object and relation b/w them 
+// resolvers
+// resolvers are the functions that determine how to fetch the data asscoiated with each field in the schema 
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+const {url} = await startStandaloneServer(server);
+console.log(`Server is ready at ${url}`);
