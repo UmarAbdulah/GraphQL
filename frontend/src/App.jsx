aed8a3d670ac88aea24a9a12ed327b4ad3abbab0
UmarAbdulah <uabdullah204@gmail.com>
2026-03-03 15:56:39 +0500

    Transaction Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import TransactionPage from "./pages/TransactionPage";
 import Header from "./components/ui/Header";
 
 function App() {
-  const authUser = false;
+  const authUser = true;
   return (
     <>
       {authUser && <Header />}
